a639d2fc9fa9563a9c068a803638c63f60d0b6e5
Hyungjin <hshin2017@gmail.com>
2026-04-18 14:27:55 -0700

    some helpful comments.

--- Runtime/RemedyTypes.cs
+++ Runtime/RemedyTypes.cs
@@ -1,7 +1,19 @@
 namespace RemedySystem
 {
-    public partial class RemedyTypes { }
-
+    /// <summary>
+    /// When calling Remedy.Log(), use RemedyTypes.ABC for quick access to all remedy types.
+    /// The Remedy source generator will create a static class with all RemedyTypes like this:
+    /// 
+    ///     public partial class RemedyTypes
+    ///     {
+    ///         public static readonly RemedyType ABC = new ABCRemedyType(); // - ABC.cs
+    ///         public static readonly RemedyType PrimaryLayout = new PrimaryLayoutRemedyType(); // - PrimaryLayout.cs
+    ///         public static readonly RemedyType UICore = new UICoreRemedyType(); // - UICore.cs
+    ///     }
+    ///
+    /// ex.
+    ///     Remedy.Log(RemedyTypes.PrimaryLayout, LogVerbosity.Normal, "This is a debug message.");
+    /// </summary>
     public abstract class RemedyType
     {
         public abstract string Name { get; }
