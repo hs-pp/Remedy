46898afeaa46a6c2e569ea68b9c6675fa9fa6ed8
Hyungjin <hshin2017@gmail.com>
2026-04-18 09:17:06 -0700

    Created Remedy package.

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # Remedy
 
-NOT READY to be moved into a Unity Package.
-
-Things to do before it's ready:
-1. Remedy Config cannot be a SO inside the plugin itself because it needs to be editable by the user. 
-   1. This creates a weird challenge with the source gen that needs to be solved.
-2. Need to add the Remedy Console.
+NOTE: Remedy cannot have runtime/editor ASMDEFs because it has a source generator that reads off of the whole codebase.
--- Runtime/Remedy.cs
+++ Runtime/Remedy.cs
@@ -1,95 +1,97 @@
-﻿using RemedySystem;
-using UnityEngine;
+﻿using UnityEngine;
 using Object = UnityEngine.Object;
 
-public static class Remedy
+namespace RemedySystem
 {
-    #region Log
-    // [AutoStaticsCleanup] Unity 6.5 feature
-    private static RemedyConfig m_config;
-    public static RemedyConfig Config // Lazy load this rather than OnInitialize in case we need logging before SystemCore is ready.
+    public static class Remedy
     {
-        get
+        #region Log
+
+        // [AutoStaticsCleanup] Unity 6.5 feature
+        private static RemedyConfig m_config;
+        public static RemedyConfig Config // Lazy load this rather than OnInitialize in case we need logging before SystemCore is ready.
         {
-            if (m_config == null)
+            get
             {
-                RemedyConfig[] configs = Resources.LoadAll<RemedyConfig>("");
-                if (configs.Length == 0)
+                if (m_config == null)
                 {
-                    Debug.LogWarning($"[Remedy] Found no RemedyConfigs in Resources folder. Please create a new one. Using temp for now.");
-                    m_config = ScriptableObject.CreateInstance<RemedyConfig>();
+                    RemedyConfig[] configs = Resources.LoadAll<RemedyConfig>("");
+                    if (configs.Length == 0)
+                    {
+                        Debug.LogWarning($"[Remedy] Found no RemedyConfigs in Resources folder. Please create a new one. Using temp for now.");
+                        m_config = ScriptableObject.CreateInstance<RemedyConfig>();
+                    }
+                    else if (configs.Length > 1)
+                    {
+                        Debug.LogWarning($"[Remedy] Found multiple RemedyConfigs in project. Loading the first one.");
+                        m_config = configs[0];
+                    }
+                    else if (configs.Length == 1)
+                    {
+                        m_config = configs[0];
+                    }
                 }
-                else if (configs.Length > 1)
-                {
-                    Debug.LogWarning($"[Remedy] Found multiple RemedyConfigs in project. Loading the first one.");
-                    m_config = configs[0];
-                }
-                else if (configs.Length == 1)
-                {
-                    m_config = configs[0];
-                }
+
+                return m_config;
+            }
+        }
+
+        [HideInCallstack] // <-- lol this shit doesnt work.
+        public static void Log(RemedyType logType, LogVerbosity verbosity, object message, Object context = null)
+        {
+            if (Config == null || !Config.EnableRemedy)
+            {
+                return;
             }
 
-            return m_config;
+            RemedyTypeSettings settings = Config.GetTypeSettings(logType);
+            if (settings == null || settings.LogVerbosity < verbosity)
+            {
+                return;
+            }
+
+            Debug.Log($"{GetLogTypeHeader(settings)} {message}", context);
         }
+
+        [HideInCallstack]
+        public static void LogWarning(RemedyType logType, LogVerbosity verbosity, object message, Object context = null)
+        {
+            if (Config == null || !Config.EnableRemedy)
+            {
+                return;
+            }
+
+            RemedyTypeSettings settings = Config.GetTypeSettings(logType);
+            if (settings == null || settings.LogVerbosity < verbosity)
+            {
+                return;
+            }
+
+            Debug.LogWarning($"{GetLogTypeHeader(settings)} {message}", context);
+        }
+
+        [HideInCallstack]
+        public static void LogError(RemedyType logType, object message, Object context = null)
+        {
+            if (Config == null || !Config.EnableRemedy)
+            {
+                return;
+            }
+
+            RemedyTypeSettings settings = Config.GetTypeSettings(logType);
+            if (settings == null)
+            {
+                return;
+            }
+
+            Debug.LogError($"{GetLogTypeHeader(settings)} {message}", context);
+        }
+
+        private static string GetLogTypeHeader(RemedyTypeSettings remedyTypeSettings)
+        {
+            return $"<color=#{ColorUtility.ToHtmlStringRGBA(remedyTypeSettings.TypeColor)}>[{remedyTypeSettings.RemedyType}] </color>";
+        }
+
+        #endregion
     }
-
-    [HideInCallstack] // <-- lol this shit doesnt work.
-    public static void Log(RemedyType logType, LogVerbosity verbosity, object message, Object context = null)
-    {
-        if (Config == null || !Config.EnableRemedy)
-        {
-            return;
-        }
-
-        RemedyTypeSettings settings = Config.GetTypeSettings(logType);
-        if (settings == null || settings.LogVerbosity < verbosity)
-        {
-            return;
-        }
-
-        Debug.Log($"{GetLogTypeHeader(settings)} {message}", context);
-    }
-
-    [HideInCallstack]
-    public static void LogWarning(RemedyType logType, LogVerbosity verbosity, object message, Object context = null)
-    {
-        if (Config == null || !Config.EnableRemedy)
-        {
-            return;
-        }
-
-        RemedyTypeSettings settings = Config.GetTypeSettings(logType);
-        if (settings == null || settings.LogVerbosity < verbosity)
-        {
-            return;
-        }
-
-        Debug.LogWarning($"{GetLogTypeHeader(settings)} {message}", context);
-    }
-
-    [HideInCallstack]
-    public static void LogError(RemedyType logType, object message, Object context = null)
-    {
-        if (Config == null || !Config.EnableRemedy)
-        {
-            return;
-        }
-        
-        RemedyTypeSettings settings = Config.GetTypeSettings(logType);
-        if (settings == null)
-        {
-            return;
-        }
-
-        Debug.LogError($"{GetLogTypeHeader(settings)} {message}", context);
-    }
-
-    private static string GetLogTypeHeader(RemedyTypeSettings remedyTypeSettings)
-    {
-        return $"<color=#{ColorUtility.ToHtmlStringRGBA(remedyTypeSettings.TypeColor)}>[{remedyTypeSettings.RemedyType}] </color>";
-    }
-
-    #endregion
-    
 }
